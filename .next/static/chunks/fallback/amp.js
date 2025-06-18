1de94c2462608a5119d3adf452c10e6ad95b53c6
Abe1888 <abebaw4040@gmail.com>
2025-06-18 14:34:30 +0300

    Added vendors-node_modules_r.js

---
+++ .next/static/chunks/fallback/amp.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["amp"],{
+
+/***/ "?506d":
+/*!******************************************!*\
+  !*** ./utils/resolve-rewrites (ignored) ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+/* (ignored) */
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors-node_modules_next_dist_b","vendors-node_modules_next_dist_client_a","vendors-node_modules_next_dist_client_components_l","vendors-node_modules_next_dist_client_components_n","vendors-node_modules_next_dist_client_components_react-dev-overlay_shared_js-0","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_ca","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_dialog_d","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_errors_call-stack_c-dc969f58","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_errors_dev-tools-in-c82b02ac","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_errors_dev-tools-in-13e6d335","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_errors_dialog_b","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_errors_en","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_errors_e","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_ho","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_o","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_components_t","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_container_b","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_d","vendors-node_modules_next_dist_client_components_react-dev-overlay_ui_s","vendors-node_modules_next_dist_client_components_react-dev-overlay_utils_c","vendors-node_modules_next_dist_client_components_redirect-","vendors-node_modules_next_dist_client_components_router-reducer_a","vendors-node_modules_next_dist_client_components_r","vendors-node_modules_next_dist_client_c","vendors-node_modules_next_dist_client_d","vendors-node_modules_next_dist_client_i","vendors-node_modules_next_dist_client_n","vendors-node_modules_next_dist_compiled_a","vendors-node_modules_next_dist_compiled_react-dom_cjs_react-dom-client_development_js-3139057c","vendors-node_modules_next_dist_compiled_react-","vendors-node_modules_next_dist_compiled_r","vendors-node_modules_next_dist_lib_c","vendors-node_modules_next_dist_p","vendors-node_modules_next_dist_shared_lib_h","vendors-node_modules_next_dist_shared_lib_m","vendors-node_modules_next_dist_shared_lib_router_router_js-58cbbd23","vendors-node_modules_next_dist_shared_lib_ro","vendors-node_modules_next_dist_shared_lib_seg","vendors-node_modules_next_dist_shared_lib_s","vendors-node_modules_react-dom_cjs_react-dom-client_development_js-a6105988","vendors-node_modules_r"], () => (__webpack_exec__("(pages-dir-browser)/./node_modules/next/dist/client/dev/amp-dev.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
